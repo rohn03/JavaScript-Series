3e40462c1aeded2b2aefdf119d4fd4b70889f635
rohn03 <73541804+rohn03@users.noreply.github.com>
2023-12-10 17:48:56 +0000

    Variables

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -3,6 +3,6 @@ let accountEmail = "rohan@gmail.com"
 var accountPassword = "123"
 accountCity = "Buldhana"
 
-console.log(accountId)
+console.log(accountId);
 
 console.table([accountId, accountEmail, accountPassword, accountCity])
