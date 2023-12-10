d28a7ccad5213afd0a5cdfbc487ec9d6a1bebb4c
rohn03 <73541804+rohn03@users.noreply.github.com>
2023-12-10 17:47:13 +0000

    Data Types

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId = 12345
+let accountEmail = "rohan@gmail.com"
+var accountPassword = "123"
+accountCity = "Buldhana"
+
+console.log(accountId)
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
