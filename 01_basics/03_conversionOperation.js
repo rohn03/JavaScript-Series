e331cb29bcb0c3a9e0232e6ca31c964b573dc431
rohn03 <73541804+rohn03@users.noreply.github.com>
2023-12-13 07:51:47 +0000

    Conversion Operations

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,14 @@
+let score = NaN
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber)
+
+
+let loggedIn = undefined
+
+let boolLoggedIn = Boolean(loggedIn);
+console.log(boolLoggedIn)
