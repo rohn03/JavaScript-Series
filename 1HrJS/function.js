d20c0e8dcfd5338eeb59c17b8aa153332103b730
rohn03 <73541804+rohn03@users.noreply.github.com>
2024-01-10 15:14:20 +0000

    functions

---
+++ 1HrJS/function.js
@@ -0,0 +1,17 @@
+function greet(){
+    console.log('HEllo')
+}
+
+function person(name){
+    console.log('Hello ' + name)
+}
+
+
+function sum(number1, number2){
+    var result = number1 + number2
+    console.log(result)
+}
+
+greet();
+person("Rohan")
+sum(2,4)
